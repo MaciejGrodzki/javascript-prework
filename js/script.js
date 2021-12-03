87e4ddff919f4218436cd8d4b25e6a974af8d9d3
Maciej Grodzki <maciejgrodzki94@gmail.com>
2021-12-03 19:14:56 +0100

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
-let computerMove ='kaimeń'
-let playerMove ='papier'
+let randomNumber = Math.floor(Math.random() * 3 + 1);
 
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-printMessage('Zagrałem ' + playerMove + ' ! Jeśli twój ruch to nożyce, to wygrywasz!')
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+    computerMove = "papier"
+}
+else if(randomNumber == 3){
+    computerMove = "nożyce"
+}
+
+printMessage('Mój ruch to: ' + computerMove);
