89404cfb019c847de7370f65efdc16d526de7194
Maciej Grodzki <maciejgrodzki94@gmail.com>
2021-12-05 13:56:57 +0100

    Add js fixes

--- js/script.js
+++ js/script.js
@@ -35,12 +35,9 @@ printMessage('Twój ruch to: ' + playerMove);
 
 if( computerMove == 'kamień' && playerMove == 'papier'){
     printMessage('Ty wygrywasz!');
-  }
+}
 else if( computerMove == 'kamień' && playerMove == 'nożyce'){
     printMessage('Przegrałeś!');
-}
-else if( computerMove == 'kamień' && playerMove == 'kamień'){
-    printMessage('Remis, Jeszcze Raz!');
 }
 else if( computerMove == 'papier' && playerMove == 'kamień'){
     printMessage('Przegrałeś!');
@@ -48,24 +45,15 @@ else if( computerMove == 'papier' && playerMove == 'kamień'){
 else if( computerMove == 'papier' && playerMove == 'nożyce'){
     printMessage('Ty wygrywasz!');
 }
-else if( computerMove == 'papier' && playerMove == 'papier'){
-    printMessage('Remis, Jeszcze Raz!');
-}
 else if( computerMove == 'nożyce' && playerMove == 'papier'){
     printMessage('Przegrałeś!');
-}
-else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
-    printMessage('Remis, Jeszcze Raz!');
 }
 else if( computerMove == 'nożyce' && playerMove == 'kamień'){
     printMessage('Ty wygrywasz!');
 }
-else if( computerMove == 'kamień' && playerMove == 'nieznany ruch'){
-    printMessage('NIE OSZUKUJ!');
+else if( computerMove == playerMove){
+    printMessage('REMIS!')
 }
-else if( computerMove == 'papier' && playerMove == 'nieznany ruch'){
-    printMessage('NIE OSZUKUJ!');
-}
-else if( computerMove == 'nożyce' && playerMove == 'nieznany ruch'){
+else if( playerMove == 'nieznany ruch'){
     printMessage('NIE OSZUKUJ!');
 }
